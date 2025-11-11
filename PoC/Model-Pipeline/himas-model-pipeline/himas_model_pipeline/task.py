ec0505c8355f9e1fc732f23383cc5069a9459615
Manjusha-26 <manjushamg26@gmail.com>
2025-11-11 10:28:01 -0500

    Fixed the dataset name to use full

--- PoC/Model-Pipeline/himas-model-pipeline/himas_model_pipeline/task.py
+++ PoC/Model-Pipeline/himas-model-pipeline/himas_model_pipeline/task.py
@@ -14,7 +14,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 # Configuration
 PROJECT_ID = "erudite-carving-472018-r5"
-DATASET_ID = "federated_demo"
+DATASET_ID = "federated"
 
 # Hospital mapping
 HOSPITAL_MAPPING = {
